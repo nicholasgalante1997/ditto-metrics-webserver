dfda04bc67ef4470cf682066725ac2c90a616ab6
Nick G <nicholasgalante1997@gmail.com>
2023-03-14 00:21:48 -0600

    time metric struct and availability metric struct:

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-use crate::models::models_mod::JSONPingHealthCheck;
+use crate::models::models_factory::AvailabilityMetric;
+use crate::models::models_factory::TimingMetric;
+use crate::models::models_factory::WebServiceHealthCheck;
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 mod models;
 
 #[get("/")]
-async fn health_check() -> impl Responder {
-    let response_body = JSONPingHealthCheck { token: String::from("health_check") };
+async fn health_check_handler() -> impl Responder {
+    let response_body = WebServiceHealthCheck {
+        token: String::from("health_check"),
+    };
     let json_ping_response = serde_json::to_string(&response_body).unwrap();
     HttpResponse::Ok().body(format!("{}", json_ping_response))
+}
+
+#[post("/availability")]
+async fn availability_handler(metric: web::Json<AvailabilityMetric>) -> impl Responder {
+    println!("*****\n{}\n*****\n", metric.build_metric_string());
+    HttpResponse::Ok()
+}
+
+#[post("/timing")]
+async fn timing_handler(metric: web::Json<TimingMetric>) -> impl Responder {
+    println!("*****\n{}\n*****\n", metric.build_metric_string());
+    HttpResponse::Ok()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(health_check)
+            .service(health_check_handler)
+            .service(availability_handler)
+            .service(timing_handler)
     })
     .bind(("127.0.0.1", 7100))?
     .run()
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,10 +1,53 @@
-pub mod models_mod {
+pub mod models_factory {
 
     use serde::{Deserialize, Serialize};
 
     #[derive(Serialize, Deserialize, Debug)]
-    pub struct JSONPingHealthCheck {
-        pub token: String
+    pub struct WebServiceHealthCheck {
+        pub token: String,
     }
 
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct AvailabilityMetric {
+        pub m_type: String,
+        pub value: AvailabilityMetricValue,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct AvailabilityMetricValue {
+        pub fn_name: String,
+        pub availability: u8,
+        pub timestamp: String,
+    }
+
+    impl AvailabilityMetric {
+        pub fn build_metric_string(&self) -> String {
+            format!(
+                "metric_type={},fn_name={},availability={},timestamp={}",
+                self.m_type, self.value.fn_name, self.value.availability, self.value.timestamp
+            )
+        }
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct TimingMetricValue {
+        pub fn_name: String,
+        pub duration: usize,
+        pub timestamp: String,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct TimingMetric {
+        pub m_type: String,
+        pub value: TimingMetricValue
+    }
+
+    impl TimingMetric {
+        pub fn build_metric_string(&self) -> String {
+            format!(
+                "metric_type={},fn_name={},duration={},timestamp={}",
+                self.m_type, self.value.fn_name, self.value.duration, self.value.timestamp
+            )
+        }
+    }
 }
